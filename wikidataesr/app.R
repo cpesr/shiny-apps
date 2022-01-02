83d5eada3cd5c535ed24ff2eb344be85bfcfb502
juliengossa <julien.gossa@gmail.com>
2022-01-02 17:57:54 +0100

    Amélioration wikidataesr

--- wikidataesr/app.R
+++ wikidataesr/app.R
@@ -10,6 +10,7 @@
 library(shiny)
 library(shinycssloaders)
 library(wikidataESR)
+library(tidyverse)
 
 # Options for Spinner
 options(spinner.color="#0275D8", spinner.color.background="#ffffff", spinner.size=2)
@@ -19,48 +20,113 @@ options(spinner.color="#0275D8", spinner.color.background="#ffffff", spinner.siz
 ui <- fluidPage(
     
     # Application title
-    titlePanel("WikidataESR: page de test"),
+    titlePanel("WikidataESR: Tenter d’y voir clair dans l’ESR"),
     
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
         sidebarPanel(width=2,
-                     textInput("wdid",
-                               "Identifiant wikidata",
-                               value = "Q2033119"),
-                     checkboxGroupInput("props", 
-                                        "Relations", 
-                                        choices = list("associé" = "associé", 
-                                                       "associé de" = "associé_de", 
-                                                       "composante" = "composante",
-                                                       "composante de" = "composante_de",
-                                                       "prédécesseur" = "prédécesseur",
-                                                       "successeur" = "successeur",
-                                                       "séparé de" = "séparé_de",
-                                                       "absorbé par" = "absorbé_par",
-                                                       "membre de" = "membre_de",
-                                                       "affilié à" = "affilié_à"
+                     textInput("wdid", "Identifiant wikidata", value = "Q2033119"),
+                     checkboxGroupInput("props", "Relations", 
+                                        choices = list("associé (P527)" = "associé", 
+                                                       "associé de (P361)" = "associé_de", 
+                                                       "composante (P355)" = "composante",
+                                                       "composante de (P749)" = "composante_de",
+                                                       "prédécesseur (P1365)" = "prédécesseur",
+                                                       "successeur (P1366)" = "successeur",
+                                                       "séparé de (P807)" = "séparé_de",
+                                                       "absorbé par (P7888)" = "absorbé_par",
+                                                       "membre de (P463)" = "membre_de",
+                                                       "affilié à (P1416)" = "affilié_à"
                                         ),
                                         selected = "composante"),
-                     sliderInput("depth",
-                                 "Profondeur",
-                                 min = 1,
-                                 max = 10,
-                                 value = 2)
-        ),
+                     sliderInput("depth", "Profondeur", min = 1, max = 10, value = 2),
+                     checkboxInput("active_only", "Actifs seulement", value = TRUE),
+                     h4("Paramètres graphiques"),
+                     sliderInput("size", "Taille (%)", min = 0, max = 300, value = 100),
+                     sliderInput("node_size", "Taille noeuds", min = 1, max = 80, value = c(40,70)),
+                     sliderInput("label_size", "Taille texte", min = 1, max = 15, value = c(4,6)),
+                     sliderInput("label_wrap", "Largeur texte", min = 10, max = 50, value = 15),
+                     selectInput("node_label", "Type de label", 
+                                 choices = list("Alias" = "alias", 
+                                                "Alias et date" = "alias_date", 
+                                                "Nom complet" = "long",
+                                                "Nom et dat" = "long_date"),
+                                 selected = "alias"),
+                     selectInput("node_type", "Type de noeud", 
+                                 choices = list("Texte" = "text",
+                                                "Texte décalé" = "text_repel", 
+                                                "Boite" = "label", 
+                                                "Boite décalée" = "label_repel"),
+                                 selected = "text"),
+                     checkboxInput("edge_label", "Labels des arcs", value = TRUE),
+                     checkboxInput("edge_arrow", "Flêche", value = FALSE),
+                     sliderInput("arrow_gap", "Distance flêche", min = 0, max = 0.3, value = 0.05, step=0.01)
+                     ),
         
         # Show a plot of the generated distribution
         mainPanel(
-            withSpinner(plotOutput("wdPlot"))
+            fluidRow(
+                withSpinner(plotOutput("wdPlot", height="600px")),
+                h3("Avertissements"),
+                h4("Avertissements pour les entités"),
+                tableOutput("vertices_warnings"),
+                h4("Avertissements pour les relations"),
+                tableOutput("edges_warnings"),
+                h2("Aide"),
+                HTML('<ul>
+                     <li><a href="https://github.com/cpesr/wikidataESR/blob/master/README.md#Guide-de-style-pour-les-données-wikidata">
+                        Explications générales</a></li>
+                     <li><a href="https://github.com/cpesr/wikidataESR/blob/master/README.md#Une-composante-UFRlabo-de-létablissement-est-manquante">
+                        Une composante (UFR/labo) de l’établissement est manquante</a></li>
+                     <li><a href="https://github.com/cpesr/wikidataESR/blob/master/README.md#Une-association-COMUE-etc-de-létablissement-est-manquante">
+                        Une association (COMUE, etc.) de l’établissement est manquante</a></li>
+                     <li><a href="https://github.com/cpesr/wikidataESR/blob/master/README.md#Une-tutelle-de-létablissement-est-manquante">
+                        Une tutelle de l’établissement est manquante</a></li>
+                     <li><a href="https://github.com/cpesr/wikidataESR/blob/master/README.md#Une-adhésion-de-létablissement-à-une-organisation-est-manquante">
+                        Une adhésion de l’établissement à une organisation est manquante</a></li>
+                     <li><a href="https://github.com/cpesr/wikidataESR/blob/master/README.md#Une-relation-composantetutelleadhésion-a-pris-fin">
+                        Une relation (composante/tutelle/adhésion) a pris fin</a></li>
+                     <li><a href="https://github.com/cpesr/wikidataESR/blob/master/README.md#Un-établissement-nexiste-plus">
+                        Un établissement n’existe plus</a></li>
+                     <li><a href="https://github.com/cpesr/wikidataESR/blob/master/README.md#Un-établissement-a-changé-de-statut">
+                        Un établissement a changé de statut</a></li>
+                     </ul>')
+            )
         )
+
     )
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
     
+    df.g <- reactive(wdesr_get_graph(input$wdid, input$props, depth = input$depth, 
+                        active_only = input$active_only))
+    
     output$wdPlot <- renderPlot({
-        wdesr_load_and_plot(input$wdid, input$props, depth = input$depth)
+        wdesr_ggplot_graph(df.g(),
+                            size = input$size / 100,
+                            node_size = input$node_size,
+                            label_size = input$label_size,
+                            label_wrap = input$label_wrap,
+                            node_label = input$node_label,
+                            node_type = input$node_type,
+                            edge_label = input$edge_label,
+                            edge_arrow =  input$edge_arrow,
+                            arrow_gap = input$arrow_gap
+                            )
     })
+    
+    output$vertices_warnings <- renderTable(
+        wdesr_log_warnings_vertices(df.g()) %>% mutate(lien = paste0("https://www.wikidata.org/wiki/",entité))
+        )
+
+    output$edges_warnings <- renderTable(
+        wdesr_log_warnings_edges(df.g()) %>% mutate(
+            depuis.lien = paste0("https://www.wikidata.org/wiki/",depuis),
+            vers.lien =  paste0("https://www.wikidata.org/wiki/",vers)) 
+    )
+    
 }
 
 # Run the application 
