227cc297490c659d2cce74aee4e295f07427d59d
juliengossa <julien.gossa@gmail.com>
2022-11-01 22:24:24 +0100

    fix bug cap

--- tdbesr/server.R
+++ tdbesr/server.R
@@ -22,26 +22,26 @@ shinyServer(function(input, output, session) {
     plots <- reactive({kpiESR::kpiesr_plot_all(rentrée, id = input$etab, groupe = input$groupe, style.k = k_style, style.o = o_style, style.o.norm = onorm_style)})
   
     output$kpi.norm <- renderPlot({ plots()$k.norm + rm_label + rL_margin + ggtitle("Indicateurs clés de performance")}, res= fig.res)
-    output$kpi.evol <- renderPlot({ plots()$k.evol + th_evol + ggtitle("Evolutions") + theme_cpesr_cap()}, res= fig.res)
+    output$kpi.evol <- renderPlot({ plots()$k.evol + th_evol + ggtitle("Evolutions") + cpesr_cap() }, res= fig.res)
     
     output$etu.abs <- renderPlot({ plots()$etu.abs + th_abs + ggtitle("Valeurs absolues")}, res= fig.res)
-    output$etu.norm <- renderPlot({ plots()$etu.norm + th_norm + ggtitle("Valeurs normalisées") + theme_cpesr_cap() }, res= fig.res)
+    output$etu.norm <- renderPlot({ plots()$etu.norm + th_norm + ggtitle("Valeurs normalisées") + cpesr_cap() }, res= fig.res)
     output$etu.evol <- renderPlot({ plots()$etu.evol + th_evol + ggtitle("Evolutions") }, res= fig.res)
 
     output$bia.abs <- renderPlot({ plots()$bia.abs + th_abs + ggtitle("Valeurs absolues") }, res= fig.res)
-    output$bia.norm <- renderPlot({ plots()$bia.norm + th_norm + ggtitle("Valeurs normalisées") + theme_cpesr_cap() }, res= fig.res)
+    output$bia.norm <- renderPlot({ plots()$bia.norm + th_norm + ggtitle("Valeurs normalisées") + cpesr_cap() }, res= fig.res)
     output$bia.evol <- renderPlot({ plots()$bia.evol + th_evol + ggtitle("Evolutions") }, res= fig.res)
 
     output$ens.abs <- renderPlot({ plots()$ens.abs + th_abs + ggtitle("Valeurs absolues") }, res= fig.res)
-    output$ens.norm <- renderPlot({ plots()$ens.norm + th_norm + ggtitle("Valeurs normalisées") + theme_cpesr_cap() }, res= fig.res)
+    output$ens.norm <- renderPlot({ plots()$ens.norm + th_norm + ggtitle("Valeurs normalisées") + cpesr_cap() }, res= fig.res)
     output$ens.evol <- renderPlot({ plots()$ens.evol + th_evol + ggtitle("Evolutions") }, res= fig.res)
     
     output$fin.abs <- renderPlot({ plots()$fin.abs + th_abs + ggtitle("Valeurs absolues") }, res= fig.res)
-    output$fin.norm <- renderPlot({ plots()$fin.norm + th_norm + ggtitle("Valeurs normalisées") + theme_cpesr_cap() }, res= fig.res)
+    output$fin.norm <- renderPlot({ plots()$fin.norm + th_norm + ggtitle("Valeurs normalisées") + cpesr_cap() }, res= fig.res)
     output$fin.evol <- renderPlot({ plots()$fin.evol + th_evol + ggtitle("Evolutions") }, res= fig.res)
 
     output$serie <- renderPlot(res= fig.res, { 
-      kpiESR::kpiesr_plot_line(id = input$etab, vars = input$serie_vars, val = input$serie_val) + theme_cpesr_cap()
+      kpiESR::kpiesr_plot_line(id = input$etab, vars = input$serie_vars, val = input$serie_val) + cpesr_cap()
     })
     
     output$map <- renderPlot(res = fig.res, { 
@@ -49,7 +49,7 @@ shinyServer(function(input, output, session) {
                               xvar = input$mapx, yvar = input$mapy ) +
         scale_x_continuous(name=kpiesr_lfc_desc[[input$mapx]], labels=kpiesr_lfc_y_labels[[input$mapx]] ) +
         scale_y_continuous(name=kpiesr_lfc_desc[[input$mapy]], labels=kpiesr_lfc_y_labels[[input$mapy]]) + 
-        theme_cpesr_cap()})
+        cpesr_cap()})
     
     choices_rentrée_map <- reactive({
       r <- kpiESR::esr %>%
